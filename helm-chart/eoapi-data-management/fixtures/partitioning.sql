5bfb0a1365da1527202d1a824fc8e68d3bdb0814
Mario Winkler <mario.winkler@dlr.de>
2025-03-25 08:27:19 +0100

    Add partitioning for S1 collections

--- helm-chart/eoapi-data-management/fixtures/partitioning.sql
+++ helm-chart/eoapi-data-management/fixtures/partitioning.sql
@@ -5,4 +5,7 @@ SET search_path = pgstac, public;
 BEGIN;
 UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l1c';
 UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l2a';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l1-grd';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l1-slc';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l2-ocn';
 COMMIT;
