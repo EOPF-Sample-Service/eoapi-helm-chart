18c2e9939107b0623e232f15feda5b094c71b359
Mario Winkler <mario.winkler@dlr.de>
2025-04-29 11:04:19 +0200

    Add Sentinel 3 collections for OLCI and SLSTR

--- helm-chart/eoapi-data-management/fixtures/partitioning.sql
+++ helm-chart/eoapi-data-management/fixtures/partitioning.sql
@@ -8,4 +8,11 @@ UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l2a';
 UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l1-grd';
 UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l1-slc';
 UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l2-ocn';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l1-efr';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l1-err';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l2-lfr';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l2-lrr';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-slstr-l1-rbt';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-slstr-l2-lst';
+
 COMMIT;
