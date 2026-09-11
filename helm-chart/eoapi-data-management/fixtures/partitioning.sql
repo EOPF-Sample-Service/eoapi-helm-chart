d00b6de0d23c5081e7695ed9e549a9577b2b9f17
Mario Winkler <mario.winkler@dlr.de>
2026-09-11 11:12:46 +0200

    Add collection for Sentinel-1 SLC Zarr3

--- helm-chart/eoapi-data-management/fixtures/partitioning.sql
+++ helm-chart/eoapi-data-management/fixtures/partitioning.sql
@@ -3,19 +3,20 @@
 --
 SET search_path = pgstac, public;
 BEGIN;
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l1c';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l2a';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l1-grd';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l1-slc';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l2-ocn';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l1-efr';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l1-err';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l2-lfr';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l2-lrr';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-slstr-l1-rbt';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-slstr-l2-lst';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-slstr-l2-frp';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l1c-zarr3';
-UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l2a-zarr3';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l1c';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l2a';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l1-grd';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l1-slc';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l2-ocn';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l1-efr';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l1-err';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l2-lfr';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-olci-l2-lrr';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-slstr-l1-rbt';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-slstr-l2-lst';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-3-slstr-l2-frp';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l1c-zarr3';
+-- UPDATE collections set partition_trunc='month' WHERE id='sentinel-2-l2a-zarr3';
+UPDATE collections set partition_trunc='month' WHERE id='sentinel-1-l1-slc-zarr3';
 
 COMMIT;
